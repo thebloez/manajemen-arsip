b555349db3143e25761b971630a47b03f7a562b4
thebloez <ryandunksfamz@gmail.com>
2019-10-27 23:21:46 +0700

    first commit from home

---
+++ application/views/inc/footer.php
@@ -0,0 +1,10 @@
+            <footer class="footer ">
+                <div class="container-fluid">
+                    <div class="copyright">
+                        Copyright &copy; <script>document.write(new Date().getFullYear())</script> SMP NEGERI 1 CIWARU
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+</body>
